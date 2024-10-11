8d68c263f6a1c587307eaa6aef1e7dd343e8c371
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-10-10 22:38:07 -0300

    feature: documentação com swagger

---
+++ api-produtos-testes-unitarios/src/main/java/carpelune/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package carpelune.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+public class OpenApiConfig {
+		
+	@Bean
+	public OpenAPI customOpenApi() {
+		return new OpenAPI()
+			.info(new Info()
+			.title("API - testes unitários com JUnit, AssertJ e Mockito")
+			.version("v1")
+			.description("API desenvolvida para a prática de testes unitários utilizando JUnit, AssertJ e Mockito")
+			.license(new License().name("Apache 2.0").url("https://github")));
+	}	
+	
+}
--- api-produtos-testes-unitarios/src/main/java/carpelune/controllers/ProductsController.java
+++ api-produtos-testes-unitarios/src/main/java/carpelune/controllers/ProductsController.java
@@ -17,30 +17,45 @@ import carpelune.dto.CreateProductDTO;
 import carpelune.dto.UpdateProductDTO;
 import carpelune.models.Product;
 import carpelune.services.ProductsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products", description = "Serviços para manipulação de produtos")
 public class ProductsController {
 	
 	@Autowired
 	ProductsService productsService;
 	
-	
+	@Operation(
+		summary = "Cria uma novo produto"
+	)
 	@PostMapping
 	public ResponseEntity<Product> createProduct(@RequestBody CreateProductDTO createProductDTO){
 		return this.productsService.createProduct(createProductDTO);
 	}
 	
+	@Operation(
+		summary = "Busca um produto por ID"
+	)
 	@GetMapping("/{productId}")
 	public ResponseEntity<Product> findProductById(@PathVariable UUID productId){
 		return this.productsService.findProductById(productId);
 	}
 	
+	@Operation(
+		summary = "Atualiza um produto existente"
+	)
+
 	@PutMapping
 	public ResponseEntity<Product> updateProduct(@RequestBody UpdateProductDTO updateProductDTO){
 		return this.productsService.updateProduct(updateProductDTO);
 	}
 	
+	@Operation(
+		summary = "Excluí um produto por ID"
+	)
 	@DeleteMapping("/{productId}")
 	public ResponseEntity<Void> deleteById(@PathVariable UUID productId){
 		return this.productsService.deleteProductById(productId);
